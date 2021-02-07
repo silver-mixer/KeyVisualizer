b08cca4658b91d040dabd57687dd2ea70635940e
silver-mixer <syumikousaku_nikki@yahoo.co.jp>
2021-02-08 06:54:44 +0900

    Add KVKey enum

---
+++ src/main/java/com/github/silver_mixer/KeyVisualizer/KVKey.java
@@ -0,0 +1,139 @@
+package com.github.silver_mixer.KeyVisualizer;
+
+public enum KVKey{
+	//Numeric Keys
+	KV_0,
+	KV_1,
+	KV_2,
+	KV_3,
+	KV_4,
+	KV_5,
+	KV_6,
+	KV_7,
+	KV_8,
+	KV_9,
+	
+	//Alphabet Keys
+	KV_A,
+	KV_B,
+	KV_C,
+	KV_D,
+	KV_E,
+	KV_F,
+	KV_G,
+	KV_H,
+	KV_I,
+	KV_J,
+	KV_K,
+	KV_L,
+	KV_M,
+	KV_N,
+	KV_O,
+	KV_P,
+	KV_Q,
+	KV_R,
+	KV_S,
+	KV_T,
+	KV_U,
+	KV_V,
+	KV_W,
+	KV_X,
+	KV_Y,
+	KV_Z,
+	
+	//Extended Keys
+	KV_BACK_QUOTE,
+	KV_MINUS,
+	KV_EQUAL,
+	KV_OPEN_BRACKET,
+	KV_CLOSE_BRACKET,
+	KV_BACK_SLASH,
+	KV_SEMICOLON,
+	KV_QUOTE,
+	KV_COMMA,
+	KV_PERIOD,
+	KV_SLASH,
+	
+	//Function Keys
+	KV_F1,
+	KV_F2,
+	KV_F3,
+	KV_F4,
+	KV_F5,
+	KV_F6,
+	KV_F7,
+	KV_F8,
+	KV_F9,
+	KV_F10,
+	KV_F11,
+	KV_F12,
+	
+	KV_PRINTSCREEN,
+	KV_PAUSE,
+	
+	//Modifier Keys
+	KV_LEFT_SHIFT,
+	KV_LEFT_CONTROL,
+	KV_LEFT_ALT,
+	KV_LEFT_WINDOWS,
+	KV_RIGHT_SHIFT,
+	KV_RIGHT_CONTROL,
+	KV_RIGHT_ALT,
+	KV_RIGHT_WINDOWS,
+	
+	//Lock Keys
+	KV_NUM_LOCK,
+	KV_CAPS_LOCK,
+	KV_SCROLL_LOCK,
+	
+	//Navigation Keys
+	KV_LFET,
+	KV_UP,
+	KV_RIGHT,
+	KV_DOWN,
+	
+	KV_PAGE_UP,
+	KV_PAGE_DOWN,
+	KV_HOME,
+	KV_END,
+	KV_ESCAPE,
+	KV_MENU,
+	
+	//Editing Keys
+	KV_ENTER,
+	KV_BACKSPACE,
+	KV_INSERT,
+	KV_DELETE,
+	KV_TAB,
+	KV_SPACE,
+	
+	//Numpad Keys
+	KV_NUM_0,
+	KV_NUM_1,
+	KV_NUM_2,
+	KV_NUM_3,
+	KV_NUM_4,
+	KV_NUM_5,
+	KV_NUM_6,
+	KV_NUM_7,
+	KV_NUM_8,
+	KV_NUM_9,
+	KV_NUM_ENTER,
+	KV_ADD,
+	KV_SUBTRACT,
+	KV_MULTIPLY,
+	KV_DIVIDE,
+	KV_DECIMAL,
+	
+	//Japanese Keys
+	KV_HANKAKU_ZENKAKU,
+	KV_KATAKANA_HIRAGANA,
+	KV_NON_CONVERSION,
+	KV_CONVERSION,
+	
+	//Japanese Extended Keys
+	KV_CIRCUMFLEX,
+	KV_YEN,
+	KV_AT,
+	KV_COLON
+}
